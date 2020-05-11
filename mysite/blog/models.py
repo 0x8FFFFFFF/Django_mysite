afc2a93502c3fc200f2be0f9b8f5ed74a65c958c
ADMIN <osmyorkin@gmail.com>
2020-05-11 14:17:24 +0300

    page 40

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'),)
     title = models.CharField(max_length=250)
@@ -13,6 +18,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager()  # Менеджер по умолчанию.
+    published = PublishedManager()  # Наш новый менеджер.
 
     class Meta:
         ordering = ('-publish',)
